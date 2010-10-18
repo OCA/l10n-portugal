899d03d5c9bcc8f9edaa3d6512542a88323efb96
cmsa <Unknown>
2010-10-18 05:40:31 +0100

    Implementação da encriptação dos campos obrigatórios para um field hash de cada factura. A encriptação é feita utilizando o módulo M2Crypt pois o pycrypto não lê directamente as chaves dos ficheiros PEM. Inclusão de uma chave pública para efeitos de testes.

--- l10n_pt_certificacao_financas/certificacao.py
+++ l10n_pt_certificacao_financas/certificacao.py
@@ -20,111 +20,69 @@
 #
 ##############################################################################
 
-##############################################################################
-#
-# 08/10/2010 - Links úteis por Carlos Almeida,
-#
-# No seguinte site são explicados alguns dos problemas que irão surgir, se se
-# utilizar o OpenSSl da forma que é sugerida:
-#  * http://www.cryptosys.net/pki/portugal_DGCI_billing_software.html
-# Existe um exemplo em Java:
-#  * http://code.google.com/p/javacertpt/
-#
-##############################################################################
-#
-# 07/10/2010 - Explicação geral feita por Paulino,
-#
-# Portaria 363/2010 e especificação técnica disponíveis em:
-#  * http://info.portaldasfinancas.gov.pt/pt/apoio_contribuinte/CertificacaoSoftware.htm
-# A certificação depende do saft - para ser certificado um programa de
-# facturação tem de ser capaz de emitir o saft. O saft é obrigatório, já a
-# certificação não é necessária em qualquer dos seguintes casos:
-#  * Software produzido internamente, do qual a entidade seja dona dos direitos
-#    de autor;
-#  * Os clientes não sejam consumidores finais - nenhum;
-#  * Vendas inferiores a € 150.000;
-#  * Número de facturas emitidas menor que 1.000 / ano.
-# Nas especificações técnicas, disponíveis na ligação acima, são apresentados
-# exemplos de como gerar as chaves publica e privada, como assinar cada factura
-# emitida com recurso ao OpenSSL.
-# Há uma módulo RSA em python para assinaturas e encriptação - http://stuvel.eu/rsa.
-# Há uma incongruência: O saft requer dois valores Hash e HashControl ambos com 40
-# caracteres. Nos exemplos dados nas especificações técnicas, o cumprimento da
-# assinatura é 172 caracteres...
-# Segundo esclarecimento que obtive será publicada uma actualização ao saft, para
-# alargar o campo Hash para 200 caracteres
-# A chave privada, necessária para assinar as facturas, é do conhecimento
-# exclusivo do produtor do software! O ficheiro que contém a chave privada,
-# deve ser convertido em binário ou encriptado (?)
-#
-##############################################################################
-
 from osv import fields
 from osv import osv
+import M2Crypto.RSA
+import binascii
 
+RSA = M2Crypto.RSA.load_pub_key("chaves/ChavePublica.pem")
 
-class account_invoice(osv.osv):
+def m2c_encrypt(InvoiceDate, SystemEntryDate, InvoiceNo, \
+                GrossTotal, LastHash=""):
+    # generate concatenated string to encrypt
+    text = InvoiceDate + ";" + SystemEntryDate + ";" + InvoiceNo + ";" + \
+           GrossTotal + ";" + LastHash
+    hash = RSA.public_encrypt(text, M2Crypto.RSA.pkcs1_padding)
+    print "Invoice Hash: [" + text + "][" + binascii.b2a_base64(hash) + "]"
+    return binascii.b2a_base64(hash)
+
+# This function is used just for testing purposes
+# should be removed later
+def m2c_decrypt(hash):
+    """decrypt(hash) --> string
+    Decrypt ciphertext (assume hex string) and return the decrypted string.
+    """
+    encrypted = binascii.a2b_base64(hash)
+    priv_key = M2Crypto.RSA.load_key("chaves/ChavePrivada.pem")
+    decrypted = priv_key.private_decrypt(encrypted, M2Crypto.RSA.pkcs1_padding)
+    return decrypted
+
+class invoice_certified_l10n_pt(osv.osv):
+    _name = "account.invoice"
     _inherit = "account.invoice"
     
     """ 
     A assinatura não acontece no estado 'draft', mas sim quando a factura é confirmada e é atribuido o numero.
     Personaliza o metodo write, quando ou apos mudar o estado para 'open'
-    A data relevante para o saft   é o 'write_date', porque a factura é criada sempre no estado 'draft' 
-    e só é verdadeiramente uma factura, quando é confirmada
+    A data relevante para o saft é o 'write_date', porque a factura é criada sempre no estado 'draft' 
+    e só é verdadeiramente uma factura, quando é confirmada.
     """
-
     def write(self, cr, uid, ids, vals, context=None):
-        
-        res = super(account_invoice, self).write(cr, uid, ids, vals, context=context)
+        #check if this is only called once!! on invoice confirmation!
         if 'state' in vals and vals['state'] == 'open':
             # todo: ler os campos que sao objecto da assinatura
-            
+            reads = self.read(cr, uid, ids, \
+                              ['id', 'date_invoice','system_entry_date', \
+                               'number', 'amount_total'])
+            if reads.id == 1:
+                # If this is the first invoice there is no last invoice hash
+                last_hash = ""
+            else:
+                last_invoice_id = reads.id - 1
+                last_invoice = self.search(cr, uid, [('id', [last_invoice_id])])
+                last_hash = last_invoice.hash
             # todo: calcular a assinatura
-
+            hash = m2c_encrypt(reads.date_invoice, reads.system_entry_date, \
+                               reads.number, reads.amount_total, \
+                               last_hash)
             # todo: Gravar a assinatura na BD
-            cr.execute("UPDATE account_invoice SET hash = " + signature
-            + " WHERE id = " + str(id) )
-        
+            self.write(cr, uid, [reads.id], {'hash': hash})
+        return super(invoice_certified_l10n_pt, self).write(cr, uid, ids, \
+                                                            vals, context)
 
-
-
-#class invoice_l10n_pt_PT_certified(osv.osv):
-#    _name = "account.invoice"
-#    _inherit = "account.invoice"
-#    _columns = {
-#        'system_entry_date': fields.datetime('System Entry Date', states={'open':[('readonly',True)],'close':[('readonly',True)]}),
-#        'hash'
-#    }
-#invoice_l10n_pt_PT_certified
-
-print "\n=====RSA 368 Demo====="
-#use 1 RSA key to encrypt the AES key
-#use another RSA key to sign AES key
-from Crypto.PublicKey import RSA
-from Crypto import Random
-
-#start the random generator
-rpool = Random.new()
-Random.atfork()
-
-# generate both RSA keys,
-privatekeyCMS = RSA.generate(1024, rpool.read)
-Random.atfork()
-privatekeyClient = RSA.generate(1024, rpool.read)
-publickeyCMS = privatekeyCMS.publickey()
-publickeyClient = privatekeyClient.publickey()
-
-#sign the AES PWD with server private key
-signed_PWD = privatekeyCMS.sign(PWD,"")
-#encrypt AES PWD with client public key
-enc_PWD = publickeyClient.encrypt(PWD, "")
-print "with publickeyClient encrypted AES-PWD:"
-print enc_PWD[0].encode("hex"),"\n"
-print "with privatekeyCMS signed AES-PWD:"
-print signed_PWD[0],"\n"
-
-#decryption
-dec_PWD= privatekeyClient.decrypt(enc_PWD[0])
-#verify identity of the
-print "key verify:\n",publickeyCMS.verify(dec_PWD,signed_PWD)
-print "decrypted PWD:\n",dec_PWD
+    _columns = {
+        'system_entry_date': fields.datetime('System Entry Date'),
+        'hash': fields.char("Invoice Hash", size=512)
+    }
+    
+invoice_certified_l10n_pt()
