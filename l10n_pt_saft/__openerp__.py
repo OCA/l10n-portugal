9f9c8c278d5602cd5712d2a6eead4dcd8091726f
Daniel Reis <dgreis@sapo.pt>
2015-05-19 10:20:19 +0100

    Package as module directory

--- l10n_pt_saft/__openerp__.py
+++ l10n_pt_saft/__openerp__.py
@@ -1,16 +1,34 @@
-# -*- coding: utf-8 -*-
+# -*- encoding: utf-8 -*-
+##############################################################################
 #
-{
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Autores:
+#    Este software baseia-se no trabalho inicial de
+#    Paulino Ascenção <paulino1@sapo.pt> (l10n_pt_saft-6)
+#    Adaptado à versão 7 por João Figueira<jjnf@communities.pt> e
+#    Jorge A. Ferreira <sysop.x0@gmail.com>
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 
-    "name" : "SAFT : implementação do saft-pt",
-    'description': """Localização para Portugal, implementação saft-pt (standard audit file for tax purposes) Ficheiro para auditoria fiscal""",
-    "version" : "0.1",
-    "author" : "Paulino Ascenção<paulino1@sapo.pt> em conjunto com a Multibase<www.multibase.pt>",
-    "website": "",
-    "category" : "Localisation/Account charts",
-    "depends" : ["account"],
-    "init_xml" : [],
-    "demo_xml" : [],
-    "update_xml": [        "saft_view.xml"  ],
-    "installable": True
+{
+    'name': 'Portugal : implementação do saft',
+    'version': '0.1',
+    'depends': ['account'],
+    'author': 'Paulino Ascenção, João Figueira, Jorge A. Ferreira',
+    'category': 'Localisation/Account charts',
+    'website': 'https://github.com/OCA/l10-portugal',
+    'data': ['saft_view.xml'],
+    'installable': True,
 }
