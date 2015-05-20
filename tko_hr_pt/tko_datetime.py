c084acf6a0955aa4ee337e0f9d6f4e52178a88a0
andreparames <github@andreparames.com>
2015-05-20 13:45:39 +0100

    Changes to comply with PEP8 and removing obsolete attributes

--- tko_hr_pt/tko_datetime.py
+++ tko_hr_pt/tko_datetime.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python
-# coding: utf-8
-########################################################################################
+# -*- coding: utf-8 -*-
+##########################################################################
 #
-# Copyright (C) ThinkOpen Solutions (<http://thinkopen.solutions>). All Rights Reserved
-# Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>). All Rights Reserved
+# Copyright (C) ThinkOpen Solutions (<http://thinkopen.solutions>).
+# All Rights Reserved
+#
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
 # License as published by the Free Software Foundation; either
 # version 3.0 of the License, or (at your option) any later version.
-# 
+#
 # This library is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # Lesser General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library.
 #
-########################################################################################
+##########################################################################
 
 """
 Extension of the datetime module to suit HRSelf project needs.
@@ -28,11 +29,12 @@ from datetime import timedelta
 import types
 import xmlrpclib
 
-DAY = DAYS = timedelta(days=1) 
+DAY = DAYS = timedelta(days=1)
 
 DATE_FORMAT = '%Y-%m-%d'
 DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
 XML_RPC_DATETIME_FORMAT = '%Y%m%dT%H:%M:%S'
+
 
 def to_date(obj, date_format=None):
     """
@@ -54,16 +56,15 @@ def to_date(obj, date_format=None):
         else:
             try:
                 res = to_date(obj, DATE_FORMAT)
-            except ValueError, e:
+            except ValueError:
                 res = to_date(obj, DATETIME_FORMAT)
     else:
-#         if isinstance(obj,bool):
-#             return False
         raise TypeError("type object %s is not date/datetime like" % type(obj))
     return res
 
+
 def to_datetime(obj, datetime_format=None):
-    """ 
+    """
     Converts an object representing a date(time) into a datetime.
     @param obj: an object representing a date(time)
     @param datetime_format: an optional datetime format
@@ -82,14 +83,15 @@ def to_datetime(obj, datetime_format=None):
         else:
             try:
                 res = to_datetime(obj, DATETIME_FORMAT)
-            except ValueError, e:
+            except ValueError:
                 res = to_datetime(obj, DATE_FORMAT)
     else:
         raise TypeError("type object %s is not date/datetime like" % type(obj))
     return res
 
+
 def to_string(obj, date_format=DATE_FORMAT, datetime_format=DATETIME_FORMAT):
-    """ 
+    """
     Converts an object representing a date(time) into a string.
     @param obj: an object representing a date(time)
     @param date_format: an optional date format
@@ -108,8 +110,10 @@ def to_string(obj, date_format=DATE_FORMAT, datetime_format=DATETIME_FORMAT):
     else:
         raise TypeError("type object %s is not date/datetime like" % type(obj))
     return res
-   
+
+
 class period(object):
+
     """Time interval between two dates (inclusive)."""
 
     def __init__(self, start, end=None):
@@ -133,7 +137,7 @@ if __name__ == '__main__':
     assert to_date("2010-01-01") == datetime.date(2010, 1, 1)
     assert to_date("2010-01-02 08:00:00") == datetime.date(2010, 1, 2)
     assert to_datetime("2010-01-03") == datetime.datetime(2010, 1, 3, 0, 0)
-    assert to_datetime("2010-01-04 08:00:00") == datetime.datetime(2010, 01, 4, 8, 0, 0)
+    assert to_datetime("2010-01-04 08:00:00") == \
+        datetime.datetime(2010, 01, 4, 8, 0, 0)
     assert to_string("2010-01-05") == "2010-01-05"
     assert to_string("2010-01-06 08:00:00") == "2010-01-06 08:00:00"
-
