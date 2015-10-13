6416bd4b86a1115f0ba82aec163bf9017b3ef9e1
Pedro Castro Silva <pedrocs@sossia.pt>
2015-10-13 10:46:30 +0100

    test_flake8 cleaning

--- l10n_pt_vat/__openerp__.py
+++ l10n_pt_vat/__openerp__.py
@@ -20,21 +20,21 @@
 ##############################################################################
 
 {
-	"name": "Portugal - IVA",
-	"version": "8.0.0.0.2",
-	"license": "AGPL-3",
-	"depends": ['account', 'base_vat'],
-	"author": "Sossia, Odoo Community Association (OCA)",
-	"summary": "Portuguese VAT requirements extensions",
-	"website": "http://www.sossia.pt",
-	"category": "Accounting & Finance",
-	"data": [
-		"security/ir.model.access.csv",
-		"data/vat_adjustment_norm.xml",
-		"views/vat_adjustment_norm_view.xml",
-		"views/account_invoice_view.xml",
-	],
-	"installable": True,
-	"auto_install": False,
-	"application": False,
+    "name": "Portugal - IVA",
+    "version": "8.0.0.0.2",
+    "license": "AGPL-3",
+    "depends": ['account', 'base_vat'],
+    "author": "Sossia, Odoo Community Association (OCA)",
+    "summary": "Portuguese VAT requirements extensions",
+    "website": "http://www.sossia.pt",
+    "category": "Accounting & Finance",
+    "data": [
+            "security/ir.model.access.csv",
+            "data/vat_adjustment_norm.xml",
+            "views/vat_adjustment_norm_view.xml",
+        "views/account_invoice_view.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+    "application": False,
 }
