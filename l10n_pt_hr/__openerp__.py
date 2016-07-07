43433dab963a5fa7865cb4eeff8b262540bdd299
Daniel Reis <dgreis@sapo.pt>
2016-07-07 10:43:58 +0100

    WIP

--- l10n_pt_hr/__openerp__.py
+++ l10n_pt_hr/__openerp__.py
@@ -1,33 +1,13 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-##########################################################################
-#
 # Copyright (C) ThinkOpen Solutions (<http://thinkopen.solutions>).
-# All Rights Reserved
-#
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public
-# License as published by the Free Software Foundation; either
-# version 3.0 of the License, or (at your option) any later version.
-#
-# This library is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public
-# License along with this library.
-#
-##########################################################################
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': 'Human Resources PT',
-    'version': '1.064',
+    'name': 'Human Resources PT Localisation',
+    'version': '8.0.2.0.0',
     'category': 'Human Resources',
-    "sequence": 38,
     'author': "ThinkOpen Solutions PT,Odoo Community Association (OCA)",
     'website': 'http://thinkopen.solutions/',
     'license': 'Other OSI approved licence',
-    'images': [],
     'depends': [
         'base', 'hr_payroll',
     ],
@@ -39,8 +19,6 @@
         'hr_payroll_pt_view.xml',
         'base_view.xml',
         'hr_config_settings_view.xml',
-    ],
-    'test': [
     ],
     'installable': True,
     'application': False,
--- l10n_pt_hr/hr_payroll_pt.py
+++ l10n_pt_hr/hr_payroll_pt.py
@@ -1,24 +1,7 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-##########################################################################
-#
 # Copyright (C) ThinkOpen Solutions (<http://thinkopen.solutions>).
-# All Rights Reserved
-#
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public
-# License as published by the Free Software Foundation; either
-# version 3.0 of the License, or (at your option) any later version.
-#
-# This library is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public
-# License along with this library.
-#
-##########################################################################
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 
 import time
 from openerp.tools.translate import _
