b9cc07106ded7f9e7d42629e415c6b50389cdada
Vimal Patel <vimal.patel.serpentcs@gmail.com>
2021-05-24 16:41:26 +0530

    [ADD] l10n_pt_account_invoicexpress: add InvoiceXpress connector

---
+++ l10n_pt_account_invoicexpress/models/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (C) 2021 Open Source Integrators
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from . import res_config_settings
+from . import account_invoicexpress
+from . import account_tax
+from . import res_partner
+from . import account_move
---
+++ l10n_pt_account_invoicexpress/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+Generate Portuguese tax authority legal Invoices ("Faturas") using InvoiceXpress.
