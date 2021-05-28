df199a1bf55c97b7b7561dcc688c24698918935d
Daniel Reis <dreis@opensourceintegrators.com>
2021-05-28 10:31:06 +0100

    [MIG] l10n_pt_vat: port to 14.0

--- l10n_pt_vat/models/account_move.py
+++ l10n_pt_vat/models/account_move.py
@@ -1,11 +1,12 @@
-# Copyright (C) 2014- Sossia, Lda. (<http://www.sossia.pt>)
+# Copyright (C) 2014 Sossia, Lda. (<http://www.sossia.pt>)
+# Copyright (C) 2021 Open SOurce Integrators (<http://www.opensourceintegrators.com>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
 
 
-class AccountInvoice(models.Model):
-    _inherit = "account.invoice"
+class AccountMove(models.Model):
+    _inherit = "account.move"
 
     vat_adjustment_norm_id = fields.Many2one(
         "account.vat.adjustment_norm",
