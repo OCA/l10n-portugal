0b623aac7e1c585ce0fd0f855e666a749cbab9a1
Daniel Reis <dreis@opensourceintegrators.com>
2021-06-04 11:36:34 +0100

    [IMP] l10n_pt_vat: add exempt reasons and fiscal zones

--- l10n_pt_vat/models/account_move.py
+++ l10n_pt_vat/models/account_move.py
@@ -2,7 +2,7 @@
 # Copyright (C) 2021 Open SOurce Integrators (<http://www.opensourceintegrators.com>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import _, api, exceptions, fields, models
 
 
 class AccountMove(models.Model):
@@ -14,3 +14,35 @@ class AccountMove(models.Model):
         ondelete="restrict",
         help="Fields 40/41 of the VAT Statement",
     )
+    l10npt_vat_exempt_reason = fields.Many2one(
+        "account.l10n_pt.vat.exempt.reason",
+        string="VAT Exempt Reason",
+        compute="_compute_l10npt_vat_exempt_reason",
+        store=True,
+        readonly=False,
+    )
+
+    @api.depends("journal_id", "company_id")
+    def _compute_l10npt_vat_exempt_reason(self):
+        for invoice in self.filtered(
+            lambda x: x.country_code == "PT" and x.is_sale_document()
+        ):
+            invoice.l10npt_vat_exempt_reason = (
+                invoice.journal_id.l10npt_vat_exempt_reason
+            )
+
+    @api.constrains("l10npt_vat_exempt_reason")
+    def _constrain_l10npt_vat_exempt_reason(self):
+        """
+        VAT Exemption reason is required if an exempt tax is used
+        """
+        for invoice in self.filtered(
+            lambda x: x.country_code == "PT" and x.is_sale_document()
+        ):
+            exempt_lines = invoice.invoice_line_ids.filtered(
+                lambda x: not x.tax_ids.filtered("amount")
+            )
+            if exempt_lines and not invoice.l10npt_vat_exempt_reason:
+                raise exceptions.ValidationError(
+                    _("A tax exemption reason must be provided.")
+                )
--- l10n_pt_vat/readme/USAGE.rst
+++ l10n_pt_vat/readme/USAGE.rst
@@ -1,1 +1,5 @@
 On an Invoice, the VAT adjustment reason is available for selection.
+
+The Fiscal Position can be used to automatically map VAT tax level
+(regular, intermediate, reduced or exempt) to the actual tax percentages,
+according to the client's region (Portugal, European Union, or others).
