ed515f2b2d86ddb2610d64009e15323238550269
Daniel Reis <dreis.pt@hotmail.com>
2021-07-05 09:44:30 +0100

    [FIX] l10n_pt_vat: fix islands VAT taxes, only set exemption reason when having exempt taxes

--- l10n_pt_vat/models/account_move.py
+++ l10n_pt_vat/models/account_move.py
@@ -14,6 +14,9 @@ class AccountMove(models.Model):
         ondelete="restrict",
         help="Fields 40/41 of the VAT Statement",
     )
+    l10npt_has_tax_exempt_lines = fields.Boolean(
+        compute="_compute_l10npt_has_tax_exempt_lines"
+    )
     l10npt_vat_exempt_reason = fields.Many2one(
         "account.l10n_pt.vat.exempt.reason",
         string="VAT Exempt Reason",
@@ -22,19 +25,33 @@ class AccountMove(models.Model):
         readonly=False,
     )
 
-    @api.depends("journal_id", "company_id")
-    def _compute_l10npt_vat_exempt_reason(self):
-        for invoice in self.filtered(
-            lambda x: x.country_code == "PT" and x.is_sale_document()
-        ):
-            invoice.l10npt_vat_exempt_reason = (
-                invoice.journal_id.l10npt_vat_exempt_reason
+    @api.depends("country_code", "move_type", "invoice_line_ids.tax_ids")
+    def _compute_l10npt_has_tax_exempt_lines(self):
+        for invoice in self:
+            invoice.l10npt_has_tax_exempt_lines = (
+                invoice.country_code == "PT"
+                and invoice.is_sale_document()
+                and invoice.invoice_line_ids.filtered(
+                    lambda x: not x.tax_ids.filtered("amount")
+                )
             )
 
-    @api.constrains("l10npt_vat_exempt_reason")
-    def _constrain_l10npt_vat_exempt_reason(self):
+    @api.depends(
+        "l10npt_has_tax_exempt_lines", "journal_id", "company_id", "amount_total"
+    )
+    def _compute_l10npt_vat_exempt_reason(self):
+        for invoice in self:
+            if (
+                invoice.l10npt_has_tax_exempt_lines
+                and not invoice.l10npt_vat_exempt_reason
+            ):
+                invoice.l10npt_vat_exempt_reason = (
+                    invoice.journal_id.l10npt_vat_exempt_reason
+                )
+
+    def action_post(self):
         """
-        VAT Exemption reason is required if an exempt tax is used
+        VAT Exemption reason is required if there are lines without tax
         """
         for invoice in self.filtered(
             lambda x: x.country_code == "PT" and x.is_sale_document()
@@ -46,3 +63,4 @@ class AccountMove(models.Model):
                 raise exceptions.ValidationError(
                     _("A tax exemption reason must be provided.")
                 )
+        return super().action_post()
