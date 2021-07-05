26178bb1df01dca8e3dc78273645a415aaa6770f
Daniel Reis <dreis.pt@hotmail.com>
2021-07-05 09:47:40 +0100

    [FIX] l10n_pt_account_invoicexpress: multicompany support, foreign country support

--- l10n_pt_account_invoicexpress/models/account_tax.py
+++ l10n_pt_account_invoicexpress/models/account_tax.py
@@ -10,15 +10,17 @@ class AccountTax(models.Model):
     invoicexpress_id = fields.Char("InvoiceXpress ID", copy=False, readonly=True)
 
     @api.model
-    def _map_invoicexpress_taxes(self):
+    def _map_invoicexpress_taxes(self, company):
         """
         Retrieves all InvoiceXpress taxes, an maps them
         to the existing Odoo taxes
         """
         InvoiceXpress = self.env["account.invoicexpress"]
-        response = InvoiceXpress.call("taxes.json", "GET")
+        response = InvoiceXpress.call(company, "taxes.json", "GET")
         invx_taxes_dict = {x["name"]: x for x in response.json().get("taxes", [])}
-        odoo_taxes = self.search([("type_tax_use", "=", "sale")])
+        odoo_taxes = self.search(
+            [("type_tax_use", "=", "sale"), ("company_id", "=", company.id)]
+        )
         for odoo_tax in odoo_taxes:
             invx_tax_vals = invx_taxes_dict.get(odoo_tax.name)
             if invx_tax_vals:
@@ -45,11 +47,11 @@ class AccountTax(models.Model):
         for tax in self.filtered(lambda x: not x.invoicexpress_id):
             payload = tax._prepare_invoicexpress_vals()
             response = InvoiceXpress.call(
-                endpoint, verb, payload=payload, raise_errors=False
+                tax.company_id, endpoint, verb, payload=payload, raise_errors=False
             )
             if response.status_code == 422:
                 # Tax name already exists, map missing invoicexpress_ids
-                self._map_invoicexpress_taxes()
+                self._map_invoicexpress_taxes(tax.company_id)
             else:
                 InvoiceXpress._check_http_status(response)
                 response_json = response.json()
