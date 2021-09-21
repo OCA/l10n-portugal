222be831d490ee9922d1049f9de0d2294447c00f
Daniel Reis <dreis.pt@hotmail.com>
2021-09-21 14:40:15 +0100

    [IMP] l10n_pt_account_invoicexpress: allow disabling for some Journals

--- l10n_pt_account_invoicexpress/models/__init__.py
+++ l10n_pt_account_invoicexpress/models/__init__.py
@@ -2,6 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from . import account_invoicexpress
+from . import account_journal
 from . import account_move
 from . import account_tax
 from . import res_company
