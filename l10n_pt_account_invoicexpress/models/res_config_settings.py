c9ccca0a18b305f09e28fc72a155b110e32b7879
Daniel Reis <dreis@opensourceintegrators.com>
2021-11-24 08:43:10 +0000

    [IMP] l10n_pt_account_invoicexpress: fix InvX Settings

--- l10n_pt_account_invoicexpress/models/res_config_settings.py
+++ l10n_pt_account_invoicexpress/models/res_config_settings.py
@@ -9,16 +9,10 @@ class ResConfigSettings(models.TransientModel):
     invoicexpress_account_name = fields.Char(
         related="company_id.invoicexpress_account_name",
         readonly=False,
-        default=lambda s: s.env["ir.config_parameter"]
-        .sudo()
-        .get_param("invoicexpress.account_name", default=""),
     )
     invoicexpress_api_key = fields.Char(
         related="company_id.invoicexpress_api_key",
         readonly=False,
-        default=lambda s: s.env["ir.config_parameter"]
-        .sudo()
-        .get_param("invoicexpress.api_key", default=""),
     )
     invoicexpress_template_id = fields.Many2one(
         related="company_id.invoicexpress_template_id", readonly=False
