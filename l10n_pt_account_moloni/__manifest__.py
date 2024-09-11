908885a97ca3d436294b33e4f3be4b670382849e
Daniel Reis <dreis@opensourceintegrators.com>
2024-09-11 11:19:16 +0100

    [ADD] l10n_pt_account_moloni: Moloni invoicing connector

---
+++ l10n_pt_account_moloni/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Moloni Invoicing",
+    "version": "14.0.1.0.0",
+    "summary": "Moloni software de facturação online",
+    "author": "ERp Gap, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/l10n-portugal",
+    "category": "Invoicing",
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "data/ir_config_parameter_data.xml",
+        "views/account_views.xml",
+        "views/res_config_settings_views.xml",
+    ],
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+}
