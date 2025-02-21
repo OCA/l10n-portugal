fdc6968cdf8cd85db2043f59baa4ac54f74c1ca4
??? <dgallo@nuobit.com>
2025-02-21 12:12:59 +0100

    [MIG] l10n_pt_stock_vehicle_daily: Backport to 14.0

--- l10n_pt_stock_vehicle_daily/__manifest__.py
+++ l10n_pt_stock_vehicle_daily/__manifest__.py
@@ -8,7 +8,7 @@
     "license": "AGPL-3",
     "author": "Luz de Airbag, Open Source Integrators, Odoo Community Association (OCA)",
     "category": "Warehouse",
-    "version": "16.0.1.0.0",
+    "version": "14.0.1.0.0",
     "depends": [
         "stock_move_location",  # from OCA/stock-logistics-warehouse
     ],
--- l10n_pt_stock_vehicle_daily/readme/CONTRIBUTORS.rst
+++ l10n_pt_stock_vehicle_daily/readme/CONTRIBUTORS.rst
@@ -1,3 +1,7 @@
 * `Open Source Integrators <https://www.opensourceintegrators.com>`_
 
     * Daniel Reis (dreis@opensourceintegrators.com)
+
+* `NuoBiT <https://www.nuobit.com>`_
+
+    * Deniz Gallo (dgallo@nuobit.com)
