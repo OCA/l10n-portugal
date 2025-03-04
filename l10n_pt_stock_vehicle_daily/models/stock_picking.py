42ec5c40295f5caaba5da1d492a3c8ca41ec8f45
??? <dgallo@nuobit.com>
2025-03-04 11:57:26 +0100

    [IMP] l10n_pt_stock_vehicle_daily: Automatically populate the corresponding license plate for the selected location

--- l10n_pt_stock_vehicle_daily/models/stock_picking.py
+++ l10n_pt_stock_vehicle_daily/models/stock_picking.py
@@ -1,10 +1,21 @@
 # Copyright 2024 Open Source Integrators
+# Copyright 2025 NuoBiT Solutions - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class StockPicking(models.Model):
     _inherit = "stock.picking"
 
-    l10n_pt_license_plate = fields.Char("License Plate")
+    l10n_pt_license_plate = fields.Char(
+        string="License Plate",
+        compute="_compute_l10n_pt_license_plate",
+        store=True,
+        readonly=False,
+    )
+
+    @api.depends("location_id")
+    def _compute_l10n_pt_license_plate(self):
+        for picking in self:
+            picking.l10n_pt_license_plate = picking.location_id.l10n_pt_license_plate
